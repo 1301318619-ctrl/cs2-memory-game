225bbf5ff70c77c55b4b97c5a5593f4e08c36b4f
1301318619-ctrl <1301318619@s.tusd1.org>
2026-03-10 21:58:07 +0000

    day 2

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 console.log("Script started")
 
+function flipCard(cardId) {
+    console.log("click");
+    let card = document.getElementById("cardId");
+    card.style.color = "peachpuff";
+    card.style.backgroundColor = "white";
+}
